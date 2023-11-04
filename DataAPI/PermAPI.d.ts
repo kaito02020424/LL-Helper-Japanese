08afc600b2d115b6d04acbe53518a0f0f0763494
kaito02020424 <kaito02020424@gmail.com>
2023-11-04 11:26:52 +0900

    Add:PernAPI translate

--- DataAPI/PermAPI.d.ts
+++ DataAPI/PermAPI.d.ts
@@ -1,152 +1,157 @@
 /**
- * 身分组实例
+ * ロールのインスタンス
  */
-declare class Role{
+declare class Role {
     /**
-     * 创建身分组
-     * @param name 身份组名称，必须是唯一的并且不能含有@#[]{}<>()/|$%^&*!~`"'+=?\n\t\r\f\v
-     * @param displayName 身份组显示名称(默认和name一样)
+     * ロールを作成します。
+     * @param name ロール名、一意である必要があり、@#[]{}<>()/|$%^&*!~`"'+=?\n\t\r\f\v を含めてはいけません
+     * @param displayName ロールの表示名（デフォルトはnameと同じ）
      */
-    constructor(name:string,displayName?:string)
+    constructor(name: string, displayName?: string);
 
-    /** 身份组名称 */
-    name:string
+    /** ロールの名前 */
+    name: string;
 
-    /** 身份组显示名称 */
-    displayName:string
+    /** ロールの表示名 */
+    displayName: string;
 
-    /** 身份组优先级，越大越优先 */
-    priority:number
+    /** ロールの優先度、値が大きいほど優先度が高い */
+    priority: number;
 
-    /** 身份组拥有的权限 */
-    permissions:Array<{name:string,enabled:boolean,extra:object}>
+    /** ロールが持つ権限 */
+    permissions: Array<{ name: string, enabled: boolean, extra: object }>;
 
-    /** 身份组成员的Xuid */
-    members:Array<string>
-
-    /**
-     * 检查身份组实例是否有效
-     */
-    isValid():boolean
+    /** ロールのメンバーのXuid */
+    members: Array<string>;
 
     /**
-     * 检查身份组是否有指定成员
-     * @param xuid 成员(玩家)的Xuid
+     * ロールインスタンスが有効かどうかを確認します。
+     * @returns {boolean} 有効な場合はtrue
      */
-    hasMember(xuid:string):boolean
+    isValid(): boolean;
 
     /**
-     * 添加成员到身份组
-     * @param xuid 成员(玩家)的Xuid
+     * ロールに指定したメンバーがいるかどうかを確認します。
+     * @param xuid メンバー（プレイヤー）のXuid
+     * @returns {boolean} メンバーがいる場合はtrue
      */
-    addMember(xuid:string):void
+    hasMember(xuid: string): boolean;
 
     /**
-     * 从身份组中移除成员
-     * @param xuid 成员(玩家)的Xuid
+     * ロールにメンバーを追加します。
+     * @param xuid メンバー（プレイヤー）のXuid
      */
-    removeMember(xuid:string):void
+    addMember(xuid: string): void;
 
     /**
-     * 检查身份组是否有指定权限
-     * @param name 权限名称
-     * @returns 是否有该权限
+     * ロールからメンバーを削除します。
+     * @param xuid メンバー（プレイヤー）のXuid
      */
-    hasPermission(name:string):boolean
+    removeMember(xuid: string): void;
 
     /**
-     * 设置身份组权限
-     * @param name 权限名称，必须已经注册在PermInfoList中
-     * @param enabled 权限是否开启
-     * @param extraData 权限的额外数据
+     * ロールが指定した権限を持っているかどうかを確認します。
+     * @param name 権限名
+     * @returns {boolean} その権限を持っている場合はtrue
      */
-    setPermission(name:string, enabled:boolean, extraData?:object):void
+    hasPermission(name: string): boolean;
 
     /**
-     * 移除身份组中的权限
-     * @param name 权限名称
+     * ロールの権限を設定します。
+     * @param name 権限名、PermInfoListに既に登録されている必要があります
+     * @param enabled 権限が有効かどうか
+     * @param extraData 権限の追加データ
      */
-    removePermission(name:string):void
+    setPermission(name: string, enabled: boolean, extraData?: object): void;
 
     /**
-     * 检查权限是否存在于身份组中
-     * @param name 权限是否已经存在于身份组中
-     * 
-     * 注意: 不同于hasPermission，这个方法会返回true只要权限已经存在于身份组，但权限不一定开启。
+     * ロールから権限を削除します。
+     * @param name 権限名
      */
-    permissionExists(name:string):boolean
+    removePermission(name: string): void;
 
+    /**
+     * 権限がロールに存在するかどうかを確認します。
+     * @param name 権限がロールに既に存在するかどうか
+     * 注意: hasPermissionと異なり、このメソッドは権限が存在する場合にtrueを返しますが、権限が必ずしも有効であるわけではありません。
+     * @returns {boolean} 権限が存在する場合はtrue
+     */
+    permissionExists(name: string): boolean;
 }
 
-declare namespace Permission{
+declare namespace Permission {
     /**
-     * Get a role
-     * @param name 身份组名称
-     * @returns 身份组实例
+     * ロールを取得します。
+     * @param name ロール名
+     * @returns {Role} ロールのインスタンス
      */
-    function getRole(name:string):Role
+    function getRole(name: string): Role;
 
     /**
-     * Get or create a role
-     * @param name 身份组名称，必须是唯一的并且不能含有@#[]{}<>()/|$%^&*!~`"'+=?\n\t\r\f\v
-     * @returns 身份组实例
+     * ロールを取得または作成します。
+     * @param name ロール名、一意である必要があり、@#[]{}<>()/|$%^&*!~`"'+=?\n\t\r\f\v を含めてはいけません
+     * @returns {Role} ロールのインスタンス
      */
-    function getOrCreateRole(name:string):Role
+    function getOrCreateRole(name: string): Role;
 
     /**
-     * 检查身份组是否存在
-     * @param name 身份组名称
+     * ロールが存在するかどうかを確認します。
+     * @param name ロール名
+     * @returns {boolean} ロールが存在する場合はtrue
      */
-    function roleExists(name:string):boolean
+    function roleExists(name: string): boolean;
 
     /**
-     * 删除身份组
-     * @param name 身份组名称
+     * ロールを削除します。
+     * @param name ロール名
      */
-    function deleteRole(name:string):void
+    function deleteRole(name: string): void;
 
     /**
-     * 注册权限
-     * @param name 权限名，唯一且不包含空格或\t\n\r\f\v，形如namespace:name(至少有一个 `:`)
-     * @param desc 权限描述
+     * 権限を登録します。
+     * @param name 権限名、一意であり、スペースまたは\t\n\r\f\vを含んではいけない、namespace:nameのような形式である必要があります（少なくとも1つの「:」を含む）
+     * @param desc 権限の説明
      */
-    function registerPermission(name:string, desc:string):void
+    function registerPermission(name: string, desc: string): void;
 
     /**
-     * 检查权限是否存在
-     * @param name 权限名称
+     * 権限が存在するかどうかを確認します。
+     * @param name 権限名
+     * @returns {boolean} 権限が存在する場合はtrue
      */
-    function permissionExists(name:string):boolean
+    function permissionExists(name: string): boolean;
 
     /**
-     * 检查玩家是否有指定权限
-     * @param xuid 玩家Xuid
-     * @param permName 权限名称
+     * プレイヤーが指定した権限を持っているかどうかを確認します。
+     * @param xuid プレイヤーのXuid
+     * @param permName 権限名
+     * @returns {boolean} プレイヤーが権限を持っている場合はtrue
      */
-    function checkPermission(xuid:string, permName:string):boolean
+    function checkPermission(xuid: string, permName: string): boolean;
 
     /**
-     * 检查玩家是否是指定身份组的成员
-     * @param xuid 玩家Xuid
-     * @param roleName 身份组名称
+     * プレイヤーが指定したロールのメンバーかどうかを確認します。
+     * @param xuid プレイヤーのXuid
+     * @param roleName ロール名
+     * @returns {boolean} プレイヤーがロールのメンバーである場合はtrue
      */
-    function isMemberOf(xuid:string, roleName:string):boolean
+    function isMemberOf(xuid: string, roleName: string): boolean;
 
     /**
-     * 获取玩家的身份组列表
-     * @param xuid 玩家Xuid
-     * @returns 此玩家的身份组列表
+     * プレイヤーのロールのリストを取得します。
+     * @param xuid プレイヤーのXuid
+     * @returns {Array<Role>} このプレイヤーのロールのリスト
      */
-    function getPlayerRoles(xuid:string):Array<Role>
+    function getPlayerRoles(xuid: string): Array<Role>;
 
     /**
-     * 获取玩家的权限列表
-     * @param xuid 玩家Xuid
+     * プレイヤーの権限リストを取得します。
+     * @param xuid プレイヤーのXuid
      */
-    function getPlayerPermissions(xuid:string):Array<object>
+    function getPlayerPermissions(xuid: string): Array<object>;
 
     /**
-     * 保存数据
+     * データを保存します。
      */
-    function saveData():void
+    function saveData(): void;
 }
