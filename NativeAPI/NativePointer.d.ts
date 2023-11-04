8eeba76b07979955c7203db73d98cf1f7333fc28
kaito02020424 <kaito02020424@gmail.com>
2023-11-04 15:56:11 +0900

    Add:NativePointer translate

--- NativeAPI/NativePointer.d.ts
+++ NativeAPI/NativePointer.d.ts
@@ -1,85 +1,101 @@
 /// <reference path="../index.d.ts" />
 
-declare class NativePointer{
+declare class NativePointer {
     /**
-     * 内存申请
-     * @param size 欲申请的内存的大小
-     * @returns NativePointer 指向新内存的指针
+     * メモリを確保します。
+     * @param size - 確保したいメモリのサイズ
+     * @returns {NativePointer} - 新しいメモリを指すポインタ
      */
-    static malloc(size:number):NativePointer;
-
+    static malloc(size: number): NativePointer;
+  
     /**
-     * 销毁内存
-     * @param block NativePointer
+     * メモリを解放します。
+     * @param block - NativePointer
      */
-    static free(block:NativePointer):void;
-
+    static free(block: NativePointer): void;
+  
     /**
-     * 指针偏移
-     * @param offset 偏移量
-     * @returns NativePointer 偏移后指针
+     * ポインタをオフセットさせます。
+     * @param offset - オフセット量
+     * @returns {NativePointer} - オフセット後のポインタ
      */
-    offset(offset:number):NativePointer;
-
+    offset(offset: number): NativePointer;
+  
     /**
-     * 获得符号地址
-     * @param symbol 需要查询的符号
-     * @returns NativePointer 查询结果，若查询失败则返回空指针
+     * シンボルからアドレスを取得します。
+     * @param symbol - 検索したいシンボル
+     * @returns {NativePointer} - 検索結果。検索に失敗した場合、空のポインタが返ります。
      */
-    fromSymbol(symbol:string):NativePointer;
-
+    fromSymbol(symbol: string): NativePointer;
+  
     /**
-     * 获得地址实例
-     * @param address 地址，以16进制字符串或数字表示
-     * @returns NativePointer 对应地址的指针实例
+     * アドレスインスタンスを取得します。
+     * @param address - アドレス、16進数の文字列または数値で表現されます
+     * @returns {NativePointer} - 対応するアドレスのポインタインスタンス
      */
-    fromAddress(address:string|number):NativePointer;
-
-    /** 指针地址 */
-    asRawAddress:number;
-
-    /** 指针地址描述 */
-    asHexAddress:string;
-
-    /** 指针内存Type */
-    byte:any;
-
-    int8:any;
-
-    uint8:any;
-
-    int16:any;
-
-    uint16:any;
-
-    int32:any;
-
-    uint32:any;
-
-    long:any;
-
-    ulong:any;
-
-    int64:any;
-
-    uint64:any;
-
-    float:any;
-
-    double:any;
-
-    string:string;
-
-    bool:boolean;
-
-    asEntity():LLSE_Entity
-
-    asItem():LLSE_Item
-
-    asPlayer():LLSE_Player
-
-    asContainer():LLSE_Container
-}
-
+    fromAddress(address: string | number): NativePointer;
+  
+    /** ポインタのアドレス */
+    asRawAddress: number;
+  
+    /** ポインタのアドレスの16進数表現 */
+    asHexAddress: string;
+  
+    /** ポインタのバイト型 */
+    byte: any;
+  
+    int8: any;
+  
+    uint8: any;
+  
+    int16: any;
+  
+    uint16: any;
+  
+    int32: any;
+  
+    uint32: any;
+  
+    long: any;
+  
+    ulong: any;
+  
+    int64: any;
+  
+    uint64: any;
+  
+    float: any;
+  
+    double: any;
+  
+    string: string;
+  
+    bool: boolean;
+  
+    /**
+     * エンティティとしてキャストします。
+     * @returns {LLSE_Entity} - エンティティオブジェクト
+     */
+    asEntity(): LLSE_Entity;
+  
+    /**
+     * アイテムとしてキャストします。
+     * @returns {LLSE_Item} - アイテムオブジェクト
+     */
+    asItem(): LLSE_Item;
+  
+    /**
+     * プレイヤーとしてキャストします。
+     * @returns {LLSE_Player} - プレイヤーオブジェクト
+     */
+    asPlayer(): LLSE_Player;
+  
+    /**
+     * コンテナとしてキャストします。
+     * @returns {LLSE_Container} - コンテナオブジェクト
+     */
+    asContainer(): LLSE_Container;
+  }
+  
 
 
