46e1c6932fda236ae21ec8453ccde8480c7f757f
kaito02020424 <kaito02020424@gmail.com>
2023-11-04 13:29:42 +0900

    Add:CommandOrigin translate

--- GameAPI/Command/CommandOrigin.d.ts
+++ GameAPI/Command/CommandOrigin.d.ts
@@ -1,34 +1,34 @@
 /// <reference path="../../index.d.ts" />
 
 /**
- * ### 🎯 命令执行者对象
+ * ### 🎯 コマンド実行者オブジェクト
  *
- * 此对象表示某次命令的执行者，通过这个对象，可以对执行者进行一些操作
+ * このオブジェクトは、コマンドの実行者を表し、実行者に対して操作を行うことができます。
  *
- * 该类**没有构造函数**，请从指令回调函数中获取
+ * このクラスには**コンストラクタはありません**。コマンドのコールバック関数から取得してください。
  *
  * @see {@linkcode Command.setCallback()}
- * @see [参数 origin ：命令的执行者](https://docs.litebds.com/zh-Hans/#/LLSEPluginDevelopment/GameAPI/Command?id=%e5%8f%82%e6%95%b0-origin-%ef%bc%9a%e5%91%bd%e4%bb%a4%e7%9a%84%e6%89%a7%e8%a1%8c%e8%80%85)
+ * @see [パラメータ origin：コマンドの実行者](https://docs.litebds.com/zh-Hans/#/LLSEPluginDevelopment/GameAPI/Command?id=%e5%8f%82%e6%95%b0-origin-%ef%bc%9a%e5%91%bd%e4%bb%a4%e7%9a%84%e6%89%a7%e8%a1%8c%e8%80%85)
  */
 declare class CommandOrigin {
-  /** 指令执行主体类型 */
+  /** コマンドの実行主体の種類 */
   readonly type: number;
 
   readonly typeName: string;
 
-  /** 指令执行主体的名称 */
+  /** コマンドの実行主体の名前 */
   readonly name: string;
 
-  /** 指令执行主体的坐标 */
+  /** コマンドの実行主体の座標 */
   readonly pos: FloatPos;
 
-  /** 指令执行主体的方块坐标 */
+  /** コマンドの実行主体のブロック座標 */
   readonly blockPos: IntPos;
 
-  /** 执行指令的实体（可空） */
+  /** コマンドを実行したエンティティ（オプション） */
   readonly entity?: Entity;
 
-  /** 执行指令的玩家（可空） */
+  /** コマンドを実行したプレイヤー（オプション） */
   readonly player?: Player;
 
   getNbt(): NbtCompound;
