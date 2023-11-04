b91329ab8cd8b701adf9cf2eb674a0174b87f6bc
kaito02020424 <kaito02020424@gmail.com>
2023-11-04 15:02:06 +0900

    Add:Form translate

--- GuiAPI/Form.d.ts
+++ GuiAPI/Form.d.ts
@@ -2,81 +2,77 @@
 
 declare class SimpleForm {
   /**
-   * 设置表单的标题
-   * @param title 表单的标题
-   * @returns SimpleForm 处理完毕的表单对象（便于连锁进行其他操作）
+   * フォームのタイトルを設定します。
+   * @param {string} title - フォームのタイトル
+   * @returns {SimpleForm} - 処理が完了したフォームオブジェクト（他の操作を連鎖できるように）: {SimpleForm}
    */
   setTitle(title: string): SimpleForm;
 
   /**
-   * 设置表单的内容
-   * @param content 表单的标题
-   * @returns SimpleForm 处理完毕的表单对象（便于连锁进行其他操作）
+   * フォームの内容を設定します。
+   * @param {string} content - フォームの内容
+   * @returns {SimpleForm} - 処理が完了したフォームオブジェクト（他の操作を連鎖できるように）: {SimpleForm}
    */
   setContent(content: string): SimpleForm;
 
   /**
-   * 向表单内增加一行按钮
-   * @param text 按钮文本的字符串
-   * @param image （可选参数）按钮图片所在路径
-   * @returns SimpleForm 处理完毕的表单对象（便于连锁进行其他操作）
+   * フォームにボタンを追加します。
+   * @param {string} text - ボタンのテキスト文字列
+   * @param {string} image （オプション） - ボタンの画像のファイルパス
+   * @returns {SimpleForm} - 処理が完了したフォームオブジェクト（他の操作を連鎖できるように）: {SimpleForm}
    */
   addButton(text: string, image?: string): SimpleForm;
 }
 
 declare class CustomForm {
   /**
-   * 设置表单的标题
-   * @param title 表单的标题
-   * @returns CustomForm 处理完毕的表单对象（便于连锁进行其他操作）
+   * フォームのタイトルを設定します。
+   * @param {string} title - フォームのタイトル
+   * @returns {CustomForm} - 処理が完了したフォームオブジェクト（他の操作を連鎖できるように）: {CustomForm}
    */
   setTitle(title: string): CustomForm;
 
   /**
-   * 向表单内增加一行文本
-   * @param text 一行文本
-   * @returns CustomForm 处理完毕的表单对象（便于连锁进行其他操作）
+   * フォームにテキストラベルを追加します。
+   * @param {string} text - 1行のテキスト
+   * @returns {CustomForm} - 処理が完了したフォームオブジェクト（他の操作を連鎖できるように）: {CustomForm}
    */
   addLabel(text: string): CustomForm;
 
   /**
-   * 向表单内增加一行输入框
-   * @param title 输入框描述文本
-   * @param placeholder （可选参数）输入框内的提示字符
-   * @param _default （可选参数）输入框中默认存在的内容
-   * @returns CustomForm 处理完毕的表单对象（便于连锁进行其他操作）
+   * フォームに入力欄を追加します。
+   * @param {string} title - 入力欄の説明テキスト
+   * @param {string} placeholder （オプション） - 入力欄内のプレースホルダー文字列
+   * @param {string} _default （オプション） - 入力欄のデフォルトの内容
+   * @returns {CustomForm} - 処理が完了したフォームオブジェクト（他の操作を連鎖できるように）: {CustomForm}
    */
   addInput(title: string, placeholder?: string, _default?: string): CustomForm;
 
   /**
-   * 向表单内增加一行开关选项
-   * @param title 开关选项描述文本
-   * @param _default （可选参数）开关的默认状态 开 / 关
-   * @returns CustomForm 处理完毕的表单对象（便于连锁进行其他操作）
+   * フォームにスイッチオプションを追加します。
+   * @param {string} title - スイッチオプションの説明テキスト
+   * @param {boolean} _default （オプション） - スイッチのデフォルトの状態（オン / オフ）
+   * @returns {CustomForm} - 処理が完了したフォームオブジェクト（他の操作を連鎖できるように）: {CustomForm}
    */
   addSwitch(title: string, _default?: boolean): CustomForm;
 
   /**
-   * 向表单内增加一行下拉菜单
-   * @param title 下拉菜单描述文本
-   * @param items 下拉菜单中的选项文本列表
-   * @param _default （可选参数）下拉菜单默认选中的列表项序号。
-   * @returns CustomForm 处理完毕的表单对象（便于连锁进行其他操作）
+   * フォームにドロップダウンメニューを追加します。
+   * @param {string} title - ドロップダウンメニューの説明テキスト
+   * @param {Array<string>} items - ドロップダウンメニュー内の選択肢のテキストリスト
+   * @param {number} _default （オプション） - ドロップダウンメニューのデフォルトで選択される項目のインデックス
+   * @returns {CustomForm} - 処理が完了したフォームオブジェクト（他の操作を連鎖できるように）: {CustomForm}
    */
-  addDropdown(
-    title: string,
-    items: Array<string>,
-    _default?: number
-  ): CustomForm;
+  addDropdown(title: string, items: Array<string>, _default?: number): CustomForm;
 
   /**
-   * 向表单内增加一行游标滑块
-   * @param title 游标滑块描述文本
-   * @param min 游标滑块最小值
-   * @param max 游标滑块最大值
-   * @param step （可选参数）游标滑块调整的最小分度值，默认为1
-   * @param _default （可选参数）游标滑块默认初始格数，数值必须在最小和最大格数之间。
-   * @returns CustomForm 处理完毕的表单对象（便于连锁进行其他操作）
+   * フォームにスライダーコントロールを追加します。
+   * @param {string} title - スライダーコントロールの説明テキスト
+   * @param {number} min - スライダーコントロールの最小値
+   * @param {number} max - スライダーコントロールの最大値
+   * @param {number} step （オプション） - スライダーコントロールの最小ステップ値、デフォルトは1
+   * @param {number} _default （オプション） - スライダーコントロールの初期値、最小値と最大値の間の数値である必要があります。
+   * @returns {CustomForm} - 処理が完了したフォームオブジェクト（他の操作を連鎖できるように）: {CustomForm}
    */
   addSlider(
     title: string,
@@ -87,10 +83,10 @@ declare class CustomForm {
   ): CustomForm;
 
   /**
-   * 向表单内增加一行步进滑块
-   * @param title 步进滑块描述文本
-   * @param items 步进滑块的选项文本列表
-   * @param _default （可选参数）步进滑块默认初始选项。序号从0开始编号
+   * フォームにステップスライダーコントロールを追加します。
+   * @param {string} title - ステップスライダーコントロールの説明テキスト
+   * @param {Array<string>} items - ステップスライダーの選択肢のテキストリスト
+   * @param {number} _default （オプション） - ステップスライダーコントロールの初期選択肢のインデックス（0から開始）
    */
   addStepSlider(
     title: string,
@@ -101,14 +97,14 @@ declare class CustomForm {
 
 declare namespace mc {
   /**
-   * 创建表单对象
-   * @returns SimpleForm 新创建的空白表单对象
+   * 新しい空白のSimpleFormオブジェクトを作成します。
+   * @returns {SimpleForm} - 新しいSimpleFormオブジェクト: {SimpleForm}
    */
   function newSimpleForm(): SimpleForm;
 
   /**
-   * 创建表单对象
-   * @returns CustomeForm 新创建的空白表单对象
+   * 新しい空白のCustomFormオブジェクトを作成します。
+   * @returns {CustomForm} - 新しいCustomFormオブジェクト: {CustomForm}
    */
   function newCustomForm(): CustomForm;
 }
