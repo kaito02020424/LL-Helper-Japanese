06a787fd199f2589e1d2fdbdf19c4956a5c5552a
kaito02020424 <kaito02020424@gmail.com>
2023-11-04 14:42:11 +0900

    Add:ScoreBoard translate

--- GameAPI/ScoreBoard.d.ts
+++ GameAPI/ScoreBoard.d.ts
@@ -1,55 +1,55 @@
 /// <reference path="../index.d.ts" />
 
 declare class Objective {
-  /** 计分项名称 */
+  /** Objectiveの名前 */
   readonly name: string;
 
-  /** 计分项显示名称 */
+  /** Objectiveの表示名 */
   readonly displayName: string;
 
   /**
-   * 获取跟踪的某个目标的分数
-   * @param target 待查询的跟踪目标，可传入玩家对象或者任意字符串
-   * @returns number 该目标/玩家在此计分项中的分数
+   * 特定の対象のスコアを取得
+   * @param target スコアを取得したい対象、プレイヤーオブジェクトまたは任意の文字列を渡すことができます
+   * @returns {number} このObjectiveでの対象/プレイヤーのスコア
    */
   getScore(target: Player | string): number;
 
   /**
-   * 修改某个目标的分数
-   * @param target 计分项跟踪的目标，可传入玩家对象或者任意字符串
-   * @param score 要设置的分数
-   * @returns number|null 该目标在经过设置操作后的分数
+   * 特定の対象のスコアを設定
+   * @param target スコアを設定したい対象、プレイヤーオブジェクトまたは任意の文字列を渡すことができます
+   * @param score 設定したいスコア
+   * @returns {number|null} 操作後の対象のスコア
    */
   setScore(target: Player | string, score: number): number | null;
 
   /**
-   * 修改某个目标的分数
-   * @param target 计分项跟踪的目标，可传入玩家对象或者任意字符串
-   * @param score 要增加的分数
-   * @returns number|null 该目标在经过增加操作后的分数
+   * 特定の対象のスコアを追加
+   * @param target スコアを追加したい対象、プレイヤーオブジェクトまたは任意の文字列を渡すことができます
+   * @param score 追加したいスコア
+   * @returns {number|null} 操作後の対象のスコア
    */
   addScore(target: Player | string, score: number): number | null;
 
   /**
-   * 修改某个目标的分数
-   * @param target 计分项跟踪的目标，可传入玩家对象或者任意字符串
-   * @param score 要减少的分数
-   * @returns number|null 该目标在经过减少操作后的分数
+   * 特定の対象のスコアを減少
+   * @param target 減少させたい対象、プレイヤーオブジェクトまたは任意の文字列を渡すことができます
+   * @param score 減少させたいスコア
+   * @returns {number|null} 操作後の対象のスコア
    */
   reduceScore(target: Player | string, score: number): number | null;
 
   /**
-   * 停止跟踪某个目标
-   * @param target 计分项跟踪的目标，可传入玩家对象或者任意字符串
-   * @returns boolean 是否停止成功
+   * 特定の対象のスコアを削除
+   * @param target 削除したい対象、プレイヤーオブジェクトまたは任意の文字列を渡すことができます
+   * @returns {boolean} 削除が成功したかどうか
    */
   deleteScore(target: Player | string): boolean;
 
   /**
-   * 设置计分项的显示状态
-   * @param slot 显示槽位名称字符串，可以为`"sidebar"`/`"belowname"`/`"list"`
-   * @param sortOrder （可选参数）排序方式，可以为`0`(升序)或`1`(降序)，默认值为`0`
-   * @returns boolean 是否设置成功
+   * Objectiveの表示設定
+   * @param slot 表示スロット名、"sidebar" / "belowname" / "list" である必要があります
+   * @param sortOrder （オプション）ソート順序、0（昇順）または1（降順）で、デフォルトは0です
+   * @returns {boolean} 設定が成功したかどうか
    */
   setDisplay(
     slot: "sidebar" | "belowname" | "list",
@@ -59,10 +59,10 @@ declare class Objective {
 
 declare namespace mc {
   /**
-   * 创建一个新的计分项
-   * @param name 计分项名称
-   * @param displayName 计分项显示名称
-   * @returns Objective|null 新增创建的计分项对象
+   * 新しいスコアObjectiveを作成
+   * @param name Objectiveの名前
+   * @param displayName Objectiveの表示名
+   * @returns {Objective|null} 新しく作成されたスコアObjectiveのオブジェクト
    */
   function newScoreObjective(
     name: string,
@@ -70,85 +70,82 @@ declare namespace mc {
   ): Objective | null;
 
   /**
-   * 获取某个已存在的计分项
-   * @param name 要获取的计分项名称
-   * @returns Objective|null 对应的计分项对象
+   * 既存のスコアObjectiveを取得
+   * @param name 取得したいスコアObjectiveの名前
+   * @returns {Objective|null} 対応するスコアObjectiveのオブジェクト
    */
   function getScoreObjective(name: string): Objective | null;
 
   /**
-   * 获取所有计分项
-   * @returns Array<Objective> 计分板系统记录的所有计分项对象
+   * すべてのスコアObjectiveを取得
+   * @returns {Array<Objective>} スコアボードシステムに記録されているすべてのスコアObjectiveオブジェクト
    */
   function getAllScoreObjectives(): Array<Objective>;
 
   /**
-   * 获取某个处于显示状态的计分项
-   * @param slot 待查询的显示槽位名称，可以为`"sidebar"`/`"belowname"`/`"list"`
-   * @returns Objective|null 正在`slot`槽位显示的计分项
+   * 表示中のスコアObjectiveを取得
+   * @param slot 取得する表示スロット名、"sidebar" / "belowname" / "list" である必要があります
+   * @returns {Objective|null} 表示中のObjective
    */
   function getDisplayObjective(
     slot: "sidbar" | "belowname" | "list"
   ): Objective | null;
 
   /**
-   * 移除一个已存在的计分项
-   * @param name 计分项名称
-   * @returns boolean 是否移除成功
+   * 既存のスコアObjectiveを削除
+   * @param name Objectiveの名前
+   * @returns {boolean} 削除が成功したかどうか
    */
   function removeScoreObjective(name: string): boolean;
 
   /**
-   * 使计分项停止显示
-   * @param slot 显示槽位名称字符串，可以为`"sidebar"`/`"belowname"`/`"list"`
-   * @returns boolean 是否清除成功
+   * スコアObjectiveの表示をクリア
+   * @param slot 表示スロット名、"sidebar" / "belowname" / "list" である必要があります
+   * @returns {boolean} クリアが成功したかどうか
    */
   function clearDisplayObjective(
     slot: "sidebar" | "belowname" | "list"
   ): boolean;
 
   /**
-   * 获取玩家计分项的分数
-   * （可查询离线玩家计分板）
+   * プレイヤーのスコアObjectiveのスコアを取得
+   * （オフラインプレイヤーのスコアボードも取得可能）
    * 
-   * @param uuid 玩家的UUID
-   * @param name 计分项名称
-   * @returns 计分板上的数值
+   * @param uuid プレイヤーのUUID
+   * @param name Objectiveの名前
+   * @returns {number} 計分ボード上の値
    */
-  function getPlayerScore(uuid:string, name:string):number
+  function getPlayerScore(uuid: string, name: string): number;
 
   /**
-   * 设置玩家计分项的分数
-   * （可修改离线玩家计分板）
-   * @param uuid 玩家的UUID 
-   * @param name 计分项名称  
-   * @param value 要设置的值
-   * @returns 是否设置成功
+   * プレイヤーのスコアObjectiveのスコアを設定
+   * （オフラインプレイヤーのスコアボードも変更可能）
+   * @param uuid プレイヤーのUUID 
+   * @param name Objectiveの名前  
+   * @param value 設定したい値
+   * @returns {boolean} 設定が成功したかどうか
    */
-  function setPlayerScore(uuid:string, name:string, value:number):boolean
+  function setPlayerScore(uuid: string, name: string, value: number): boolean;
 
   /**
-   * 增加玩家计分项的分数
-   * （可修改离线玩家计分板）
-   * @param uuid 玩家的UUID 
-   * @param name 计分项名称  
-   * @param value 要增加的值
-   * @returns 是增加置成功
+   * プレイヤーのスコアObjectiveのスコアを増加
+   * （オフラインプレイヤーのスコアボードも変更可能）
+   * @param uuid プレイヤーのUUID 
+   * @param name Objectiveの名前  
+   * @param value 増加したい値
+   * @returns {boolean} 増加が成功したかどうか
    */
-  function addPlayerScore(uuid:string, name:string, value:number):boolean
+  function addPlayerScore(uuid: string, name: string, value: number): boolean;
 
   /**
-   * 减少玩家计分项的分数
-   * （可修改离线玩家计分板）
-   * @param uuid 玩家的UUID 
-   * @param name 计分项名称  
-   * @param value 要减少的值
-   * @returns 是否减少成功
+   * プレイヤーのスコアObjectiveのスコアを減少
+   * （オフラインプレイヤーのスコアボードも変更可能）
+   * @param uuid プレイヤーのUUID 
+   * @param name Objectiveの名前  
+   * @param value 減少させたい値
+   * @returns {boolean} 減少が成功したかどうか
    */
-  function reducePlayerScore(uuid:string, name:string, value:number):boolean
-
-
-
+  function reducePlayerScore(uuid: string, name: string, value: number): boolean;
 }
 
 declare class LLSE_Objective extends Objective{}
