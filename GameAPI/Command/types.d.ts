c697879627c7f76d0414124b1ccf6e093122f193
kaito02020424 <kaito02020424@gmail.com>
2023-11-04 13:34:56 +0900

    Add:types translate

--- GameAPI/Command/types.d.ts
+++ GameAPI/Command/types.d.ts
@@ -1,89 +1,89 @@
 /// <reference path="../../index.d.ts" />
 
 /**
- * ### 指令执行所需权限枚举
+ * ### コマンド実行に必要な権限の列挙型
  *
- * @see [注册一条顶层命令](https://docs.litebds.com/zh-Hans/#/LLSEPluginDevelopment/GameAPI/Command?id=%e6%b3%a8%e5%86%8c%e4%b8%80%e6%9d%a1%e9%a1%b6%e5%b1%82%e5%91%bd%e4%bb%a4)
+ * @see [トップレベルコマンドの登録](https://docs.litebds.com/zh-Hans/#/LLSEPluginDevelopment/GameAPI/Command?id=%e6%b3%a8%e5%86%8c%e4%b8%80%e6%9d%a1%e9%a1%b6%e5%b1%82%e5%91%bd%e4%bb%a4)
  * @see {@linkcode mc.newCommand()}
  * @see https://github.com/LiteLDev/LiteLoaderBDS/blob/main/LiteLoader/Header/MC/Command.hpp#L17
  */
 declare enum PermType {
-  /** 任何人都可以执行这条指令 */
+  /** 任意のユーザーがこのコマンドを実行できます */
   Any = 0,
 
-  /** 只有OP可以执行这条指令（默认值） */
+  /** OPのみがこのコマンドを実行できます（デフォルト値） */
   GameMasters = 1,
 
   Admin = 2,
 
   HostPlayer = 3,
 
-  /** 只有控制台可以执行这条指令 */
+  /** コンソールのみがこのコマンドを実行できます */
   Console = 4,
 
   Internal = 5,
 }
 
 /**
- * ### 命令参数类型
+ * ### コマンドパラメータの型
  *
- * @see [有效的指令参数及其解释](https://docs.litebds.com/zh-Hans/#/LLSEPluginDevelopment/GameAPI/Command?id=%e6%9c%89%e6%95%88%e7%9a%84%e5%91%bd%e4%bb%a4%e5%8f%82%e6%95%b0%e7%b1%bb%e5%9e%8b%e5%8f%8a%e8%a7%a3%e9%87%8a)
+ * @see [有効なコマンドパラメータとその解説](https://docs.litebds.com/zh-Hans/#/LLSEPluginDevelopment/GameAPI/Command?id=%e6%9c%89%e6%95%88%e7%9a%84%e5%91%bd%e4%bb%a4%e5%8f%82%e6%95%b0%e7%b1%bb%e5%9e%8b%e5%8f%8a%e8%a7%a3%e9%87%8a)
  * @see [`Command.mandatory()`](Command.d.ts)
  * @see [`Command.optional()`](Command.d.ts)
  */
 declare enum ParamType {
-  /** `boolean` - 布尔值参数 */
+  /** `boolean` - ブール値パラメータ */
   Bool,
 
-  /** `number` - 整数参数 */
+  /** `number` - 整数パラメータ */
   Int,
 
-  /** `number` - 浮点数参数 */
+  /** `number` - 浮動小数点数パラメータ */
   Float,
 
-  /** `string` - 字符串参数 */
+  /** `string` - 文字列パラメータ */
   String,
 
-  /** {@linkcode Entity} - 实体目标选择器参数 */
+  /** {@linkcode Entity} - エンティティターゲットセレクターパラメータ */
   Actor,
 
-  /** {@linkcode Player} - 玩家目标选择器参数 */
+  /** {@linkcode Player} - プレイヤーターゲットセレクターパラメータ */
   Player,
 
-  /** {@linkcode IntPos} - 整数坐标参数 */
+  /** {@linkcode IntPos} - 整数座標パラメータ */
   BlockPos,
 
-  /** {@linkcode FloatPos} - 浮点数坐标参数 */
+  /** {@linkcode FloatPos} - 浮動小数点数座標パラメータ */
   Vec3,
 
-  /** `string` - 原始字符串参数（可包含特殊字符，如逗号空格） */
+  /** `string` - 生の文字列パラメータ（コンマやスペースなどの特殊文字を含むことができます） */
   RawText,
 
-  /** `string` - 消息类型参数（同 `/say` 指令参数，会自动展开目标选择器等） */
+  /** `string` - メッセージタイプパラメータ（`/say` コマンドのようなパラメータで、ターゲットセレクターなどが自動的に展開されます） */
   Message,
 
-  /** `string`  - `Json`字符串参数 */
+  /** `string` - `Json`文字列パラメータ */
   JsonValue,
 
-  /** [`Item`](../Item.d.ts) - 物品类型参数 */
+  /** [`Item`](../Item.d.ts) - アイテムタイプパラメータ */
   Item,
 
-  /** [`Block`](../Block/Block.d.ts) - 方块类型参数 */
+  /** [`Block`](../Block/Block.d.ts) - ブロックタイプパラメータ */
   Block,
 
-  /** `string`  - 效果类型参数 */
+  /** `string` - エフェクトタイプパラメータ */
   Effect,
 
-  /** `string`  - 枚举参数 */
+  /** `string` - 列挙型パラメータ */
   Enum,
 
-  /** `string`  - 可变枚举参数 */
+  /** `string` - 可変列挙型パラメータ */
   SoftEnum,
 
-  /** `string`  - 实体类型参数 */
+  /** `string` - エンティティタイプパラメータ */
   ActorType,
 
-  /** `string`  - 指令名称参数（仅供测试） */
+  /** `string` - コマンド名パラメータ（テスト用） */
   Command,
 
   /** `null` - WildcardCommandSelector\<Actor\> */
@@ -91,9 +91,9 @@ declare enum ParamType {
 }
 
 /**
- * ### 指令执行主体类型
+ * ### コマンド実行主体の型
  *
- * @see [参数 origin ：命令的执行者](https://docs.litebds.com/zh-Hans/#/LLSEPluginDevelopment/GameAPI/Command?id=%e5%8f%82%e6%95%b0-origin-%ef%bc%9a%e5%91%bd%e4%bb%a4%e7%9a%84%e6%89%a7%e8%a1%8c%e8%80%85)
+ * @see [引数 origin: コマンドの実行者](https://docs.litebds.com/zh-Hans/#/LLSEPluginDevelopment/GameAPI/Command?id=%e5%8f%82%e6%95%b0-origin-%ef%bc%9a%e5%91%bd%e4%bb%a4%e7%9a%84%e6%89%a7%e8%a1%8c%e8%80%85)
  * @see {@linkcode CommandOrigin.type}
  * @see https://github.com/LiteLDev/LiteLoaderBDS/blob/main/LiteLoader/Header/MC/Command.hpp#L22
  */
@@ -127,9 +127,9 @@ declare enum ParamOption {
 
   EnumAutocompleteExpansion = 1,
 
-  /** be used in block or item name enum */
+  /** ブロックまたはアイテム名の列挙型で使用 */
   HasSemanticConstraint = 2,
 
-  /** be used in NewExecuteCommand */
+  /** NewExecuteCommandで使用 */
   EnumAsChainedCommand = 4,
 }
