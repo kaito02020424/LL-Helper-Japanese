989f5f7e8eedabf9d4cb1f34e03387c7d386ab60
kaito02020424 <kaito02020424@gmail.com>
2023-11-04 16:03:27 +0900

    Add:index translate

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
 
-// Data数据文件
+// Dataファイル
 /// <reference path="DataAPI/OtherData.d.ts" />
 /// <reference path="DataAPI/PlayerData.d.ts" />
 /// <reference path="DataAPI/Economy.d.ts" />
@@ -8,43 +8,43 @@
 /// <reference path="DataAPI/DataBase.d.ts" />
 /// <reference path="DataAPI/PermAPI.d.ts" />
 
-// GameAPI文件
+// GameAPIファイル
 /// <reference path="GameAPI/index.d.ts" />
 
-// EventAPI 文件
+// EventAPIファイル
 /// <reference path="EventAPI/BlockEvents.d.ts" />
 /// <reference path="EventAPI/EconomicEvents.d.ts" />
 /// <reference path="EventAPI/EntityEvents.d.ts" />
 /// <reference path="EventAPI/OtherEvents.d.ts" />
 /// <reference path="EventAPI/PlayerEvents.d.ts" />
 
-// GuiAPI 文件
+// GuiAPIファイル
 /// <reference path="GuiAPI/Form.d.ts" />
 
-// NbtAPI 文件
+// NbtAPIファイル
 /// <reference path="NbtAPI/NBTCompound.d.ts" />
 
-// ScriptAPI 文件
+// ScriptAPIファイル
 /// <reference path="ScriptAPI/Li.d.ts" />
 /// <reference path="ScriptAPI/Logger.d.ts" />
 /// <reference path="ScriptAPI/ScriptHelper.d.ts" />
 /// <reference path="ScriptAPI/i18n.d.ts" />
 
-// NbtAPI 文件
+// NbtAPIファイル
 /// <reference path="NbtAPI/NBT.d.ts" />
 /// <reference path="NbtAPI/NBTCompound.d.ts" />
 /// <reference path="NbtAPI/NBTList.d.ts" />
 /// <reference path="NbtAPI/NBTValue.d.ts" />
 
-// SystemAPI 文件
+// SystemAPIファイル
 /// <reference path="SystemAPI/File.d.ts" />
 /// <reference path="SystemAPI/Network.d.ts" />
 /// <reference path="SystemAPI/SystemCall.d.ts" />
 
-// NativeAPI文件
+// NativeAPIファイル
 /// <reference path="NativeAPI/NativeFunction.d.ts" />
 /// <reference path="NativeAPI/NativePointer.d.ts" />
 /// <reference path="NativeAPI/NativePatch.d.ts" />
 
-// 个体文件
+// 个体ファイル
 /// <reference path="Other.d.ts" />
