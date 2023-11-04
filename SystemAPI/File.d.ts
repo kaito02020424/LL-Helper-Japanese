13d2b1e8a6caaa9090d9f2ea0375d5831fbe28af
kaito02020424 <kaito02020424@gmail.com>
2023-11-04 15:46:11 +0900

    Add:File translate

--- SystemAPI/File.d.ts
+++ SystemAPI/File.d.ts
@@ -2,92 +2,92 @@
 
 declare class file {
   /**
-   *
-   * @param path 想要打开的文件路径
-   * @param mode 文件的打开模式
-   * @param isBinary （可选参数）是否以二进制模式打开文件，默认为`false`
+   * ファイルを表すクラスです。
+   * @param path - 想要打开的文件路径
+   * @param mode - ファイルの打開モード
+   * @param isBinary - （オプション）二進モードでファイルを開く場合はtrue。デフォルトは`false`
    */
   constructor(path: string, mode: number, isBinary?: boolean);
 
-  /** 当前文件路径 */
+  /** 現在のファイルのパス */
   readonly path: string;
 
-  /** 当前文件的绝对路径 */
+  /** 現在のファイルの絶対パス */
   readonly absolutePath: string;
 
-  /** 当前文件大小 */
+  /** 現在のファイルのサイズ */
   readonly size: number;
 
-  /** 文件打开模式枚举 - 只读 */
+  /** ファイルのオープンモードを示す列挙型（読み取り専用） */
   static readonly ReadMode: number;
 
-  /** 文件打开模式枚举 - 写入 */
+  /** ファイルのオープンモードを示す列挙型（書き込み） */
   static readonly WriteMode: number;
 
-  /** 文件打开模式枚举 - 追加 */
+  /** ファイルのオープンモードを示す列挙型（追加） */
   static readonly AppendMode: number;
 
   /**
-   * 读入文件的所有内容
-   * @param path 目标文件的路径，相对路径以BDS根目录为基准
-   * @returns string 文件的所有数据
+   * ファイルの内容を読み込みます。
+   * @param path - 対象ファイルのパス。相対パスはBDSルートディレクトリを基準とします。
+   * @returns {string | null} - ファイルの内容または読み込みに失敗した場合はnull
    */
   static readFrom(path: string): string | null;
 
   /**
-   * 向指定文件写入内容
-   * @param path 目标文件的路径，相对路径以BDS根目录为基准
-   * @param text 要写入的内容
-   * @returns boolean 是否写入成功
+   * 指定のファイルに内容を書き込みます。
+   * @param path - 対象ファイルのパス。相対パスはBDSルートディレクトリを基準とします。
+   * @param text - 書き込む内容
+   * @returns {boolean} - 書き込みに成功した場合はtrue、それ以外はfalse
    */
   static writeTo(path: string, text: string): boolean;
 
   /**
-   * 向指定文件追加一行
-   * @param path 目标文件的路径，相对路径以BDS根目录为基准
-   * @param text 要写入的内容
-   * @returns boolean 是否写入成功
+   * 指定のファイルに行を追加します。
+   * @param path - 対象ファイルのパス。相対パスはBDSルートディレクトリを基準とします。
+   * @param text - 追加する行の内容
+   * @returns {boolean} - 書き込みに成功した場合はtrue、それ以外はfalse
    */
   static writeLine(path: string, text: string): boolean;
 
   /**
-   * 从文件读取文本 / 二进制数据
-   * @param cnt 要读取的字符数 / 字节数
-   * @returns string|ByteBuffer 读取的字符串内容 / 二进制数据
+   * ファイルからテキストまたはバイナリデータを読み取ります。
+   * @param cnt - 読み取る文字数またはバイト数
+   * @returns {string | ByteBuffer | null} - 読み取った文字列の内容、バイナリデータ、または読み取りに失敗した場合はnull
    */
   readSync(cnt: number): string | ByteBuffer | null;
 
   /**
    * 从文件读取一行文本
-   * @returns string|null 读取的字符串内容
+   * @returns {string|null} 读取的字符串内容
    */
   readLineSync(): string | null;
 
   /**
-   * 从文件读取所有内容
-   * @returns string|ByteBuffer 读取的字符串内容 / 二进制数据
+   * ファイルからすべての内容を読み取ります。
+   * @returns {string | ByteBuffer | null} - 読み取った文字列の内容、バイナリデータ、または読み取りに失敗した場合はnull
    */
   readAllSync(): string | ByteBuffer | null;
 
   /**
-   * 写入文本 / 二进制数据到文件
-   * @param str 要写入的内容
-   * @returns boolean 是否成功写入
+   * テキストまたはバイナリデータをファイルに書き込みます。
+   * @param str - 書き込む内容
+   * @returns {boolean} - 書き込みに成功した場合はtrue、それ以外はfalse
    */
   writeSync(str: string | ByteBuffer): boolean;
 
   /**
-   * 写入一行文本到文件
-   * @param str 要写入的内容
-   * @returns boolean 是否成功写入
+   * ファイルに一行のテキストを書き込みます。
+   * @param str - 書き込む内容
+   * @returns {boolean} - 書き込みに成功した場合はtrue、それ以外はfalse
    */
   writeLineSync(str: string): boolean;
 
   /**
-   * 从文件读取文本 / 二进制数据（异步）
-   * @param cnt 要读取的字符数 / 字节数
-   * @param callback 获取结果的回调函数
-   * @returns boolean 是否成功发送请求
+   * ファイルからテキストまたはバイナリデータを非同期で読み取ります。
+   * @param cnt - 読み取る文字数またはバイト数
+   * @param callback - 結果を取得するためのコールバック関数
+   * @returns {boolean} - リクエストの送信に成功した場合はtrue、それ以外はfalse
    */
   read(
     cnt: number,
@@ -95,24 +95,24 @@ declare class file {
   ): boolean;
 
   /**
-   * 从文件读取一行文本（异步）
-   * @param callback 获取结果的回调函数
-   * @returns boolean 是否成功发送请求
+   * ファイルから一行のテキストを非同期で読み取ります。
+   * @param callback - 結果を取得するためのコールバック関数
+   * @returns {boolean} - リクエストの送信に成功した場合はtrue、それ以外はfalse
    */
   readLine(callback: (result: string) => void): boolean;
 
   /**
-   * 从文件读取所有内容（异步）
-   * @param callback 获取结果的回调函数
-   * @returns boolean 是否成功发送请求
+   * ファイルからすべての内容を非同期で読み取ります。
+   * @param callback - 結果を取得するためのコールバック関数
+   * @returns {boolean} - リクエストの送信に成功した場合はtrue、それ以外はfalse
    */
   readAll(callback: (result: string | ByteBuffer | null) => void): boolean;
 
   /**
-   * 写入文本 / 二进制数据到文件（异步）
-   * @param str 要写入的内容
-   * @param callback （可选参数）获取结果的回调函数
-   * @returns boolean 是否成功发送请求
+   * テキストまたはバイナリデータをファイルに非同期で書き込みます。
+   * @param str - 書き込む内容
+   * @param callback - （オプション）結果を取得するためのコールバック関数
+   * @returns {boolean} - リクエストの送信に成功した場合はtrue、それ以外はfalse
    */
   write(
     str: string | ByteBuffer,
@@ -120,10 +120,10 @@ declare class file {
   ): boolean;
 
   /**
-   * 写入一行文本到文件（异步）
-   * @param str 要写入的内容
-   * @param callback （可选参数）获取结果的回调函数
-   * @returns boolean 是否成功发送请求
+   * ファイルに一行のテキストを非同期で書き込みます。
+   * @param str - 書き込む内容
+   * @param callback - （オプション）結果を取得するためのコールバック関数
+   * @returns {boolean} - リクエストの送信に成功した場合はtrue、それ以外はfalse
    */
   writeLine(
     str: string | ByteBuffer,
@@ -131,125 +131,125 @@ declare class file {
   ): boolean;
 
   /**
-   * 移动文件指针
-   * @param pos 文件指针要移动到的位置
-   * @param isRelative 是否是相对当前文件指针位置移动
-   * @returns boolean 是否移动成功
+   * ファイルポインタを移動します。
+   * @param pos - ファイルポインタを移動する位置
+   * @param isRelative - 現在のファイルポインタ位置からの相対位置かどうか
+   * @returns {boolean} - 移動に成功した場合はtrue、それ以外はfalse
    */
   seekTo(pos: number, isRelative: boolean): boolean;
 
   /**
-   * 设定文件大小
-   * @param size 要设定的目标大小
-   * @returns boolean 是否设定成功
-   * @tips 设定的新大小可以大于文件的当前大小。如果设定的新大小小于文件的当前大小，原文件将被截断。
+   * ファイルのサイズを設定します。
+   * @param size - 設定する目標のサイズ
+   * @returns {boolean} - 設定に成功した場合はtrue、それ以外はfalse
+   * @tips - 設定された新しいサイズは、ファイルの現在のサイズより大きい場合があります。新しいサイズがファイルの現在のサイズより小さい場合、元のファイルは切り詰められます。
    */
   setSize(size: number): boolean;
 
   /**
-   * 关闭文件
-   * @returns boolean 是否成功关闭
-   * @tips 文件关闭后，严禁再次使用
+   * ファイルを閉じます。
+   * @returns {boolean} - 閉じるのに成功した場合はtrue、それ以外はfalse
+   * @tips ファイルを閉じた後、再度使用しないでください。
    */
   close(): boolean;
 
   /**
-   * 文件指针是否位于文件尾
-   * @returns boolean 文件指针是否处于文件尾
+   * ファイルポインタがファイルの末尾にあるかどうかを返します。
+   * @returns {boolean} - ファイルポインタがファイルの末尾にある場合はtrue、それ以外はfalse
    */
   isEOF(): boolean;
 
   /**
-   * 刷新文件缓冲区
-   * @returns boolean 是否成功刷新
+   * ファイルバッファをフラッシュします。
+   * @returns {boolean} - フラッシュに成功した場合はtrue、それ以外はfalse
    */
   flush(): boolean;
 
   /**
-   * 获取错误码
-   * @returns Integer 上一次IO操作产生的错误码
+   * 前回のIO操作で生成されたエラーコードを取得します。
+   * @returns {number} - 前回のIO操作で生成されたエラーコード
    */
   errorCode(): number;
 
   /**
-   * 清除错误状态
-   * @return boolean 是否成功清除
+   * エラー状態をクリアします。
+   * @return boolean - クリアに成功した場合はtrue、それ以外はfalse
    */
   clear(): boolean;
 
   /**
-   * 创建文件夹
-   * @param dir 目标文件夹的路径
-   * @returns boolean 是否成功创建
+   * フォルダを作成します。
+   * @param dir - ターゲットフォルダのパス
+   * @returns {boolean} - 作成に成功した場合はtrue、それ以外はfalse
    */
   static createDir(dir: string): boolean;
 
   /**
-   * 创建文件夹
-   * @param dir 目标文件夹的路径
-   * @returns boolean 是否成功创建
+   * フォルダを作成します（エイリアス）。
+   * @param dir - ターゲットフォルダのパス
+   * @returns {boolean} - 作成に成功した場合はtrue、それ以外はfalse
    */
   static mkdir(dir: string): boolean;
 
   /**
-   * 删除文件 / 文件夹
-   * @param path 目标文件 / 文件夹的路径
-   * @returns boolean 是否成功删除
+   * ファイルまたはフォルダを削除します。
+   * @param path - ターゲットファイルまたはフォルダのパス
+   * @returns {boolean} - 削除に成功した場合はtrue、それ以外はfalse
    */
   static delete(path: string): boolean;
 
   /**
-   * 判断文件 / 文件夹是否存在
-   * @param path 目标文件 / 文件夹的路径
-   * @returns boolean 目标是否存在
+   * ファイルまたはフォルダが存在するかどうかを判断します。
+   * @param path - ターゲットファイルまたはフォルダのパス
+   * @returns {boolean} - ターゲットが存在する場合はtrue、それ以外はfalse
    */
   static exists(path: string): boolean;
 
   /**
-   * 复制文件 / 文件夹到指定位置
-   * @param from 源文件 / 文件夹的路径
-   * @param to 目标文件 / 文件夹的位置
-   * @returns boolean 是否复制成功
+   * ファイルまたはフォルダを指定の場所にコピーします。
+   * @param from - ソースファイルまたはフォルダのパス
+   * @param to - ターゲットファイルまたはフォルダの場所
+   * @returns {boolean} - コピーに成功した場合はtrue、それ以外はfalse
    */
   static copy(from: string, to: string): boolean;
 
   /**
-   * 移动文件 / 文件夹到指定位置
-   * @param from 源文件 / 文件夹的路径
-   * @param to 目标文件 / 文件夹的位置
-   * @returns boolean 是否复制成功
+   * ファイルまたはフォルダを指定の場所に移動します。
+   * @param from - ソースファイルまたはフォルダのパス
+   * @param to - ターゲットファイルまたはフォルダの場所
+   * @returns {boolean} - 移動に成功した場合はtrue、それ以外はfalse
    */
   static move(from: string, to: string): boolean;
 
   /**
-   * 重命名指定文件 / 文件夹
-   * @param from 文件 / 文件夹的旧名字
-   * @param to 新名字
-   * @returns boolean 是否复制成功
+   * 指定のファイルまたはフォルダをリネームします。
+   * @param from - ファイルまたはフォルダの古い名前
+   * @param to - 新しい名前
+   * @returns {boolean} - リネームに成功した場合はtrue、それ以外はfalse
    */
   static rename(from: string, to: string): boolean;
 
   /**
-   * 获取指定文件的大小
-   * @param path 所操作的文件路径
-   * @returns Integer 文件的大小（字节）
-   * @tips 如果传入的路径位置是一个文件夹，则返回`-1`
+   * 指定のファイルのサイズを取得します。
+   * @param path - 操作対象のファイルのパス
+   * @returns {number} - ファイルのサイズ（バイト単位）
+   * @tips 渡されたパスがディレクトリの場合、-1 を返します。
    */
   static getFileSize(path: string): number;
 
   /**
-   * 判断指定路径是否是文件夹
-   * @param path 所判断的路径
-   * @returns boolean 目标路径是否是文件夹
+   * 指定のパスがディレクトリであるかどうかを判定します。
+   * @param path - 判定対象のパス
+   * @returns {boolean} - 対象のパスがディレクトリの場合はtrue、それ以外はfalse
    */
   static checkIsDir(path: string): boolean;
 
   /**
-   * 列出指定文件夹下的所有文件 / 文件夹
-   * @param dir 文件夹路径
-   * @returns Array<string> 文件夹名数组
+   * 指定のディレクトリ内のすべてのファイルとフォルダをリストアップします。
+   * @param dir - フォルダのパス
+   * @returns {string[]} - フォルダ内のファイルおよびフォルダ名の配列
    */
-  static getFilesList(dir: string): Array<string>;
+  static getFilesList(dir: string): string[];
 }
 
-declare class File extends file {}
+declare class File extends file { }
