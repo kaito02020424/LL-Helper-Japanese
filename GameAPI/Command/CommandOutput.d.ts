285664e212d8d434d44cb115f9380568dbf93142
kaito02020424 <kaito02020424@gmail.com>
2023-11-04 13:31:23 +0900

    Add:CommandOutput translate

--- GameAPI/Command/CommandOutput.d.ts
+++ GameAPI/Command/CommandOutput.d.ts
@@ -1,14 +1,14 @@
 /// <reference path="../../index.d.ts" />
 
 /**
- * ### 🎯 命令输出对象
+ * ### 🎯 CommandOutputオブジェクト
  *
- * 通过这个对象，可以向命令执行者输出命令的执行结果。
+ * このオブジェクトを使用することで、コマンドの実行結果をコマンドの実行者に出力できます。
  *
- * 该类**没有构造函数**，请从指令回调函数中获取
+ * このクラスには**コンストラクタ**がありません。コマンドコールバック関数から取得してください。
  *
  * @see {@linkcode Command.setCallback()}
- * @see [参数 output ：向命令执行者输出命令的执行结果](https://docs.litebds.com/zh-Hans/#/LLSEPluginDevelopment/GameAPI/Command?id=%e5%8f%82%e6%95%b0-output-%ef%bc%9a%e5%90%91%e5%91%bd%e4%bb%a4%e6%89%a7%e8%a1%8c%e8%80%85%e8%be%93%e5%87%ba%e5%91%bd%e4%bb%a4%e7%9a%84%e6%89%a7%e8%a1%8c%e7%bb%93%e6%9e%9c)
+ * @see [パラメータ output：コマンドの実行結果をコマンドの実行者に出力する](https://docs.litebds.com/zh-Hans/#/LLSEPluginDevelopment/GameAPI/Command?id=%e5%8f%82%e6%95%b0-output-%ef%bc%9a%e5%90%91%e5%91%bd%e4%bb%a4%e6%89%a7%e8%a1%8c%e8%80%85%e8%be%93%e5%87%ba%e5%91%bd%e4%bb%a4%e7%9a%84%e6%89%a7%e8%a1%8c%e7%bb%93%e6%9e%9c)
  */
 declare class CommandOutput {
   readonly empty: boolean;
@@ -16,39 +16,39 @@ declare class CommandOutput {
   readonly successCount: number;
 
   /**
-   * ### 输出一条成功信息
+   * ### 成功メッセージを出力
    * 
-   * @param msg 要输出的信息
+   * @param msg 出力するメッセージ
    * 
-   * @param param 要替换的参数
+   * @param param 置換するパラメータ
    * 
-   * @returns 是否成功输出
+   * @returns 成功したかどうか
    */
   success(msg?: string, param?: any[]): boolean;
 
   /**
-   * ### 输出一条错误信息
+   * ### エラーメッセージを出力
    * 
-   * @param msg 要输出的信息
+   * @param msg 出力するメッセージ
    * 
-   * @param param 要替换的参数
+   * @param param 置換するパラメータ
    * 
-   * @returns 是否成功输出
+   * @returns 成功したかどうか
    */
   error(msg: string, param?: any[]): boolean;
 
   /**
-   * ### 输出一条普通信息
+   * ### 通常のメッセージを出力
    * 
-   * @param msg 要输出的信息
+   * @param msg 出力するメッセージ
    * 
-   * @param param 要替换的参数
+   * @param param 置換するパラメータ
    * 
-   * @param arg3 整数 作用未知
+   * @param arg3 未知の整数 未使用
    * 
-   * @returns 是否成功输出
+   * @returns 成功したかどうか
    */
-  addMessage(msg: string, param?: any[], arg3?:number): boolean;
+  addMessage(msg: string, param?: any[], arg3?: number): boolean;
 
   toString(): string;
 }
