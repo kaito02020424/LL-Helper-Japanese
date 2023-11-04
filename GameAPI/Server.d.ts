1cd6ecb27c69d1208090bcac1ae3271a7a70f596
kaito02020424 <kaito02020424@gmail.com>
2023-11-04 14:43:04 +0900

    Add:Server translate

--- GameAPI/Server.d.ts
+++ GameAPI/Server.d.ts
@@ -1,27 +1,27 @@
-declare namespace mc{
+declare namespace mc {
     /**
-     * 获取BDS服务端版本号
-     * @returns string 服务端版本号字符串，格式形如`v1.17.10`
+     * BDS サーバーバージョンを取得
+     * @returns {string} サーバーバージョン文字列、フォーマットは `v1.17.10` のような形式です
      */
-    function getBDSVersion():string;
+    function getBDSVersion(): string;
 
     /**
-     * 获取BDS服务端协议版本
-     * @returns number 服务端协议版本
+     * BDS サーバープロトコルバージョンを取得
+     * @returns {number} サーバープロトコルバージョン
      */
-     function getServerProtocolVersion():number;
+    function getServerProtocolVersion(): number;
 
     /**
-     * 设置服务器Motd字符串  
-     * @param motd 目标Motd字符串  
-     * @returns boolean 是否设置成功
+     * サーバーの MOTD 文字列を設定
+     * @param motd 目標の MOTD 文字列
+     * @returns {boolean} 設定が成功したかどうか
      */
-    function setMotd(motd:string):boolean;
+    function setMotd(motd: string): boolean;
 
     /**
-     * 设置服务器最大玩家数
-     * @param num 最大玩家数
-     * @returns boolean 是否设置成功
+     * サーバーの最大プレイヤー数を設定
+     * @param num 最大プレイヤー数
+     * @returns {boolean} 設定が成功したかどうか
      */
-    function setMaxPlayers(num:number):boolean;
-}
+    function setMaxPlayers(num: number): boolean;
+}  
