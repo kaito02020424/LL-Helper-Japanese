7cb32add057bf8a46664e0e92ea42417ec6bb0ce
kaito02020424 <kaito02020424@gmail.com>
2023-11-04 13:26:12 +0900

    Add:BlockEntity translate

--- GameAPI/BlockEntity/BlockEntity.d.ts
+++ GameAPI/BlockEntity/BlockEntity.d.ts
@@ -1,57 +1,57 @@
 /// <reference path="../../index.d.ts" />
 
 /**
- * ### 方块实体对象
+ * ### ブロックエンティティオブジェクト
  *
- * 在LLSE中，使用「方块实体对象」来操作和获取与特定方块关联的附加数据
+ * LLSEでは、「ブロックエンティティオブジェクト」を使用して特定のブロックに関連する追加データを操作および取得します。
  *
- * 该类**没有构造函数**，请通过其他方式获得类对象
+ * このクラスには**コンストラクタが存在しません**。クラスオブジェクトを取得するには他の方法を使用してください。
  *
- * **注意**：**方块实体对象** 不是一种 **实体**！他们之间并没有特别的关系
+ * **注意**: **ブロックエンティティオブジェクト**は **実体(Entity)** ではありません！\
+ * それらの間に特別な関係はありません。
  *
- * **注意**：不要长期保存一个方块实体对象\
- * 当方块对象对应的方块被销毁时，对应的方块实体对象将变得无效。\
- * 因此，如果有长期操作某个方块实体的需要，请通过{@linkcode Block.getBlockEntity()}获取实时的方块实体对象
+ * **注意**: ブロックエンティティオブジェクトを**長期間保持しないでください**。\
+ * ブロックに対応するブロックが破壊されると、対応するブロックエンティティオブジェクトも無効になります。\
+ * したがって、特定のブロックエンティティを長期間操作する必要がある場合は、**{@linkcode Block.getBlockEntity()}**を使用してリアルタイムのブロックエンティティオブジェクトを取得してください。
  */
 declare class BlockEntity {
 
-  /** 方块实体的名称 */
+  /** ブロックエンティティの名前 */
   readonly name: string 
 
-  /** 方块实体对应方块所在的坐标 */
+  /** ブロックエンティティに対応するブロックの位置 */
   readonly pos: IntPos;
 
-  /** 方块实体对象的类型ID */
+  /** ブロックエンティティオブジェクトのタイプID */
   readonly type: number;
 
   /**
-   * ### 获取方块实体对应的NBT对象
+   * ### ブロックエンティティに対応するNBTオブジェクトを取得
    *
-   * 关于NBT对象的更多使用，请参考 [NBT接口文档](https://docs.litebds.com/#/zh_CN/Development/NbtAPI/NBT)
+   * NBTオブジェクトについての詳細は、[NBTインターフェースドキュメント](https://docs.litebds.com/#/zh_CN/Development/NbtAPI/NBT)を参照してください。
    *
-   * @returns 方块实体的NBT对象
+   * @returns ブロックエンティティのNBTオブジェクト
    */
   getNbt(): NbtCompound;
 
   /**
-   * ### 写入方块实体对应的NBT对象
+   * ### ブロックエンティティに対応するNBTオブジェクトを書き込む
    *
-   * 关于NBT对象的更多使用，请参考 [NBT接口文档](https://docs.litebds.com/#/zh_CN/Development/NbtAPI/NBT)
+   * NBTオブジェクトについての詳細は、[NBTインターフェースドキュメント](https://docs.litebds.com/#/zh_CN/Development/NbtAPI/NBT)を参照してください。
    *
-   * @param nbt NBT对象
-   *
-   * @returns 是否成功写入
+   * @param nbt NBTオブジェクト
+   * @returns 書き込みに成功したかどうか
    */
   setNbt(nbt: NbtCompound): boolean;
 
   /**
-   * ### 获取方块实体对应的方块对象
+   * ### ブロックエンティティに対応するブロックオブジェクトを取得
    *
-   * @returns 方块实体对应的方块对象
+   * @returns ブロックエンティティに対応するブロックオブジェクト
    */
   getBlock(): Block;
 
   asPointer(): NativePointer;
 }
 
-declare class LLSE_BlockEntity extends BlockEntity{}
+declare class LLSE_BlockEntity extends BlockEntity {}
