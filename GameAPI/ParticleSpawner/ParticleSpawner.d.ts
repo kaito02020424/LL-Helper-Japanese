192f4d5271b4f462f7d9d955d54c8704c8bc97a8
kaito02020424 <kaito02020424@gmail.com>
2023-11-04 14:45:16 +0900

    Add:PartivleSpawner translate

--- GameAPI/ParticleSpawner/ParticleSpawner.d.ts
+++ GameAPI/ParticleSpawner/ParticleSpawner.d.ts
@@ -1,107 +1,116 @@
 /// <reference path="../../index.d.ts" />
 /**
- * ### ✨ 粒子生成器
+ * ### ✨ パーティクル生成クラス
  * 
- * 该类**没有构造函数**，  
- * 请通{@linkcode mc.newParticleSpawner()}过获得类对象
+ * このクラスには**コンストラクタ**が存在しません。  
+ * クラスのオブジェクトを取得するには {@linkcode mc.newParticleSpawner()} を使用してください。
  * 
- * @see [✨ ParticleSpawner 对象](https://docs.litebds.com/zh-Hans/#/LLSEPluginDevelopment/GameAPI/Particle)
+ * @see [✨ ParticleSpawner オブジェクト](https://docs.litebds.com/zh-Hans/#/LLSEPluginDevelopment/GameAPI/Particle)
  */
-declare class ParticleSpawner{
-    /** 粒子显示半径 */
-    displayRadius:number
+declare class ParticleSpawner {
+    /** パーティクルの表示半径 */
+    displayRadius: number;
 
-    /** 需要高细节线条 */
-    highDetial:boolean
+    /** 高詳細なラインを描画する必要があるかどうか */
+    highDetail: boolean;
 
-    /** 需要双面粒子 */
-    doubleSide:boolean
-
+    /** 両面パーティクルが必要かどうか */
+    doubleSided: boolean;
 
     /**
-     * ### 生成指定名称粒子
+     * ### 指定した名前のパーティクルを生成
      * 
-     * @param pos 可以是浮点坐标或者整数坐标, 整数坐标会取方块中心点位置
-     * @param name 
+     * @param pos 浮動小数点座標または整数座標。整数座標の場合、ブロックの中心座標が使用されます。
+     * @param name パーティクルの名前
      */
-    spawnParticle(pos: FloatPos | IntPos, name:string):boolean
+    spawnParticle(pos: FloatPos | IntPos, name: string): boolean;
 
 
     /**
-     * ### 生成点粒子
+     * ### ポイントパーティクルを生成
      * 
-     * @param pos 可以是浮点坐标或者整数坐标, 整数坐标会取方块中心点位置
-     * @param pointSize 只有 1, 2, 4, 8, 16 大小可选 默认为 `4`
-     * @param color 应当使用 ParticleColor 枚举填充 默认为 `ParticleColor.White`
+     * @param pos 浮動小数点座標または整数座標。整数座標の場合、ブロックの中心座標が使用されます。
+     * @param pointSize サイズオプション：1、2、4、8、16から選択（デフォルト：4）
+     * @param color パーティクルの色（デフォルト：ParticleColor.White）
      */
-    drawPoint(pos: FloatPos | IntPos, pointSize?:1|2|4|8|16, color?:ParticleColor):boolean
+    drawPoint(pos: FloatPos | IntPos, pointSize?: 1 | 2 | 4 | 8 | 16, color?: ParticleColor): boolean;
 
 
     /**
-     * ### 生成数字粒子
+     * ### 数字パーティクルを生成
      * 
-     * @param pos 可以是浮点坐标或者整数坐标, 整数坐标会取方块中心点位置
-     * @param num 若为Number只可填 0\~16, 若为String可填 `0`\~`16` 或 `A`\~`F`
-     * @param color 应当使用 ParticleColor 枚举填充 默认为 `ParticleColor.White`
+     * @param pos 浮動小数点座標または整数座標。整数座標の場合、ブロックの中心座標が使用されます。
+     * @param num 数字（Number型：0〜16までまたはString型："0"〜"16"または"A"〜"F"）
+     * @param color パーティクルの色（デフォルト：ParticleColor.White）
      */
-    drawNumber(pos: FloatPos | IntPos, num: number | string, color?:ParticleColor):boolean
+    drawNumber(pos: FloatPos | IntPos, num: number | string, color?: ParticleColor): boolean;
 
 
     /**
-     * ### 生成轴向线段
+     * ### 軸方向のラインパーティクルを生成
      * 
-     * @param pos 线段起点, 可以是浮点坐标或者整数坐标, 整数坐标会取方块中心点位置
-     * @param direction 应当使用 Direction 枚举填充
-     * @param length 任意实数
-     * @param color 应当使用 ParticleColor 枚举填充 默认为 `ParticleColor.White`
+     * @param pos 開始位置（浮動小数点座標または整数座標。整数座標の場合、ブロックの中心座標が使用されます）
+     * @param direction 方向（Direction列挙体を使用）
+     * @param length 任意の実数
+     * @param color パーティクルの色（デフォルト：ParticleColor.White）
      */
-    drawAxialLine(pos: FloatPos | IntPos, direction:Direction, length:number, color?:ParticleColor):boolean
+    drawAxialLine(pos: FloatPos | IntPos, direction: Direction, length: number, color?: ParticleColor): boolean;
 
 
     /**
-     * ### 生成有向线段
+     * ### 方向付きのラインパーティクルを生成
      * 
-     * @param start 线段起点, 可以是浮点坐标或者整数坐标, 整数坐标会取方块中心点位置
-     * @param end 线段终点, 可以是浮点坐标或者整数坐标, 整数坐标会取方块中心点位置
-     * @param lineWidth 只有 1, 2, 4, 8, 16 大小可选 默认值 `4`
-     * @param minSpacing 任意实数, 此线段点最小间隔 默认值 `1`
-     * @param maxParticlesNum 整数, 此线段最大粒子数，达到后会自动增加间隔 默认值 `64`
-     * @param color 应当使用 ParticleColor 枚举填充 默认值 `ParticleColor.White`
+     * @param start 開始位置（浮動小数点座標または整数座標。整数座標の場合、ブロックの中心座標が使用されます）
+     * @param end 終了位置（浮動小数点座標または整数座標。整数座標の場合、ブロックの中心座標が使用されます）
+     * @param lineWidth サイズオプション：1、2、4、8、16から選択（デフォルト：4）
+     * @param minSpacing 任意の実数（ラインセグメント間の最小スペース、デフォルト：1）
+     * @param maxParticlesNum 整数（ラインの最大パーティクル数、達成後、自動的に間隔が増加します、デフォルト：64）
+     * @param color パーティクルの色（デフォルト：ParticleColor.White）
      */
-    drawOrientedLine(start: FloatPos | IntPos, end: FloatPos | IntPos, lineWidth?:1|2|4|8|16, minSpacing?:number, maxParticlesNum?:number, color?:ParticleColor):boolean
-
+    drawOrientedLine(
+        start: FloatPos | IntPos,
+        end: FloatPos | IntPos,
+        lineWidth?: 1 | 2 | 4 | 8 | 16,
+        minSpacing?: number,
+        maxParticlesNum?: number,
+        color?: ParticleColor
+    ): boolean;
 
     /**
-     * ### 生成圆
-     * @param pos 圆心, 可以是浮点坐标或者整数坐标, 整数坐标会取方块中心点位置
-     * @param facing 应当使用 Direction 枚举填充
-     * @param radius 任意实数, 半径
-     * @param lineWidth 只有 1, 2, 4, 8, 16 大小可选 默认值 `4`
-     * @param minSpacing 任意实数, 此线段点最小间隔 默认值 `1`
-     * @param maxParticlesNum 整数, 此线段最大粒子数，达到后会自动增加间隔 默认值 `64`
-     * @param color 当使用 ParticleColor 枚举填充 默认值 `ParticleColor.White`
+     * ### 円を生成
+     * @param pos 円の中心位置（浮動小数点座標または整数座標。整数座標の場合、ブロックの中心座標が使用されます）
+     * @param facing 方向（Direction列挙体を使用）
+     * @param radius 任意の実数（半径）
+     * @param lineWidth サイズオプション：1、2、4、8、16から選択（デフォルト：4）
+     * @param minSpacing 任意の実数（ラインセグメント間の最小スペース、デフォルト：1）
+     * @param maxParticlesNum 整数（ラインの最大パーティクル数、達成後、自動的に間隔が増加します、デフォルト：64）
+     * @param color パーティクルの色（デフォルト：ParticleColor.White）
      */
-    drawCircle(pos: FloatPos | IntPos, facing: Direction, radius:number, lineWidth?:1|2|4|8|16, minSpacing?:number, maxParticlesNum?:number, color?:ParticleColor):boolean
-
-
-
-
+    drawCircle(
+        pos: FloatPos | IntPos,
+        facing: Direction,
+        radius: number,
+        lineWidth?: 1 | 2 | 4 | 8 | 16,
+        minSpacing?: number,
+        maxParticlesNum?: number,
+        color?: ParticleColor
+    ): boolean;
 
     /**
-     * ### 生成立方体
-     * @param pos 可以是浮点坐标或者整数坐标, 整数坐标会取方块角落底部位置, 若只有一个坐标，会画出 1×1×1 大小的立方体
-     * @param pos2 可以是浮点坐标或者整数坐标, 整数坐标会取方块角落顶端位置, 会画出从最小角落 pos 到最大角落 pos2 的立方体
-     * @param color 应当使用 ParticleColor 枚举填充 默认值 `ParticleColor.White`
+     * ### 立方体を生成
+     * @param pos 開始位置（浮動小数点座標または整数座標。整数座標の場合、ブロックの角の底部位置が使用されます。1つの座標の場合、1×1×1の立方体が描画されます）
+     * @param pos2 終了位置（浮動小数点座標または整数座標。整数座標の場合、ブロックの角の上部位置が使用されます。posからpos2までの立方体が描画されます）
+     * @param color パーティクルの色（デフォルト：ParticleColor.White）
      */
-    drawCuboid(pos: FloatPos | IntPos ,pos2?: FloatPos | IntPos, color?:ParticleColor):boolean
+    drawCuboid(pos: FloatPos | IntPos, pos2?: FloatPos | IntPos, color?: ParticleColor): boolean;
 
 
 }
 
 /**
- * 粒子颜色枚举
+ * パーティクルの色の列挙型
  */
-declare enum ParticleColor{
+declare enum ParticleColor {
     Black,
     Indigo,
     Lavender,
@@ -121,13 +130,13 @@ declare enum ParticleColor{
 }
 
 /**
- * 方向枚举
+ * 方向の列挙型
  */
-declare enum Direction{
+declare enum Direction {
     NEG_Y,
     POS_Y,
     NEG_Z,
     POS_Z,
     NEG_X,
     POS_X
-}
+}  
