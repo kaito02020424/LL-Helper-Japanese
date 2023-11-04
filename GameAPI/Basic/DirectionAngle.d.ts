9e783c6424c4b7b880ec430d03dbc65a1fac5532
kaito02020424 <kaito02020424@gmail.com>
2023-11-04 11:30:58 +0900

    Add:DirectionAngle translate

--- GameAPI/Basic/DirectionAngle.d.ts
+++ GameAPI/Basic/DirectionAngle.d.ts
@@ -1,33 +1,33 @@
 /// <reference path="../../index.d.ts" />
 
 /**
- * ### 📐 方向角对象
+ * ### 📐 方向角オブジェクト
  *
- * 引擎采用 `DirectionAngle` 对象来标示一个欧拉角，称之为「方向角对象」\
- * 它的两个成员均为浮点数，多用来表示实体的朝向等方向数据
+ * エンジンは「DirectionAngle」オブジェクトを使用してオイラー角を示し、「方向角オブジェクト」と呼びます。
+ * このオブジェクトの2つのメンバーは、通常エンティティの向きなどの方向データを表すのに使用される浮動小数点数です。
  *
- * @see [📐 方向角对象](https://docs.litebds.com/zh-Hans/#/LLSEPluginDevelopment/GameAPI/Basic?id=%f0%9f%93%90-%e6%96%b9%e5%90%91%e8%a7%92%e5%af%b9%e8%b1%a1)
+ * @see [📐 方向角オブジェクト](https://docs.litebds.com/zh-Hans/#/LLSEPluginDevelopment/GameAPI/Basic?id=%f0%9f%93%90-%e6%96%b9%e5%90%91%e8%a7%92%e5%af%b9%e8%b1%a1)
  */
 declare class DirectionAngle {
   /**
-   * @param pitch 俯仰角（-90° ~ 90°）
-   * @param yaw 偏航角（旋转角）
+   * @param pitch 仰角（-90° ~ 90°）
+   * @param yaw 偏角（回転角）
    */
   constructor(pitch: number, yaw: number);
 
-  /** 俯仰角（-90° ~ 90°） */
+  /** 仰角（-90° ~ 90°） */
   pitch: number;
 
-  /** 偏航角（旋转角） */
+  /** 偏角（回転角） */
   yaw: number;
 
   /**
-   * ### 将偏航角转换为基本朝向
+   * ### 偏角を基本的な方向に変換する
    *
-   * 返回值为`0`-`3`，代表 北东南西 四个基本朝向\
-   * 用于快速确定实体面向的大致方向
+   * 返り値は`0`-`3`で、北東南西の4つの基本的な方向を表します。
+   * エンティティがどの方向を向いているかを素早く判断するのに使用されます。
    *
-   * @returns 当前方向角对象所指示的基本朝向
+   * @returns 現在の方向角オブジェクトが指す基本的な方向
    */
   toFacing(): 0 | 1 | 2 | 3;
 
