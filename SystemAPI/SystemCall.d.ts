d6179acba2fb335b42eadcc203803b14c60667ac
kaito02020424 <kaito02020424@gmail.com>
2023-11-04 15:52:05 +0900

    Add:SystemCall translate

--- SystemAPI/SystemCall.d.ts
+++ SystemAPI/SystemCall.d.ts
@@ -1,70 +1,70 @@
 /// <reference path="../index.d.ts" />
 
-declare class time{
-	/** 年份数值（4位） */
-	Y:number;
-
-	/** 月份数值 */
-	M:number;
-
-	/** 天数数值 */
-	D:number;
-
-	/** 小时数值（24小时制） */
-	h:number;
-
-	/** 分钟数值 */
-	m:number;
-
-	/** 秒数值 */
-	s:number;
-
-	/** 毫秒数值 */
-	ms:number;
-}
-
-declare namespace system {
-  /**
-   * 调用shell执行指定系统命令
-   * @param cmd 执行的系统命令
-   * @param callback shell进程结束之后返回数据使用的回调函数
-   * @param timeLimit （可选参数）命令运行的最长时限，单位为毫秒  默认为`-1`，即不限制运行时间
-   * @returns boolean 是否成功启动命令
-   */
-  function cmd(
-    cmd: string,
-    callback: (exitcode: number, output: string) => void,
-    timeLimit?: number
-  ): boolean;
-
-  /**
-   * 运行指定位置程序
-   * @param process 运行的程序路径（与命令行参数）
-   * @param callback 程序进程结束之后返回数据使用的回调函数
-   * @param timeLimit （可选参数）程序进程运行的最长时限，单位为毫秒  默认为`-1`，即不限制运行时间
-   * @returns boolean 是否成功启动命令
-   */
-  function newProcess(
-    process: string,
-    callback: (exitcode: number, output: string) => void,
-    timeLimit?: number
-  ): boolean;
-
+declare class Time {
+	/** 年の数値（4桁） */
+	Y: number;
+  
+	/** 月の数値 */
+	M: number;
+  
+	/** 日の数値 */
+	D: number;
+  
+	/** 時間の数値（24時間制） */
+	h: number;
+  
+	/** 分の数値 */
+	m: number;
+  
+	/** 秒の数値 */
+	s: number;
+  
+	/** ミリ秒の数値 */
+	ms: number;
+  }
+  
+  declare namespace system {
 	/**
-	 * 获取当前时间字符串  
-	 * @returns string 当前的时间字符串，使用当地时区和24小时制。 
+	 * シェルを呼び出して指定されたシステムコマンドを実行します。
+	 * @param cmd - 実行するシステムコマンド
+	 * @param callback - シェルプロセスが終了した後にデータを返すコールバック関数
+	 * @param timeLimit - （オプション）コマンドの最大実行時間（ミリ秒単位）。デフォルトは`-1`で、制限時間がありません。
+	 * @returns {boolean} - コマンドの起動に成功したかどうか
 	 */
-	function getTimeStr():string;
-
+	function cmd(
+	  cmd: string,
+	  callback: (exitcode: number, output: string) => void,
+	  timeLimit?: number
+	): boolean;
+  
 	/**
-	 * 获取当前的时间对象
-	 * @returns timeObject 当前的时间对象
+	 * 指定されたプロセスを実行します。
+	 * @param process - 実行するプログラムのパス（およびコマンドライン引数）
+	 * @param callback - プログラムプロセスが終了した後にデータを返すコールバック関数
+	 * @param timeLimit - （オプション）プログラムプロセスの最大実行時間（ミリ秒単位）。デフォルトは`-1`で、制限時間がありません。
+	 * @returns {boolean} - プロセスの起動に成功したかどうか
 	 */
-	function getTimeObj():time;
-
+	function newProcess(
+	  process: string,
+	  callback: (exitcode: number, output: string) => void,
+	  timeLimit?: number
+	): boolean;
+  
 	/**
-	 * 随机生成一个 GUID  字符串
-	 * @returns string 一个随机生成的唯一标识符GUID
+	 * 現在の時間文字列を取得します。
+	 * @returns {string} - 現在の時間文字列。現地のタイムゾーンと24時間制を使用します。
 	 */
-	function randomGuid():string;
-}
+	function getTimeStr(): string;
+  
+	/**
+	 * 現在の時間オブジェクトを取得します。
+	 * @returns {Time} - 現在の時間オブジェクト
+	 */
+	function getTimeObj(): Time;
+  
+	/**
+	 * ランダムなGUID（グローバル一意識別子）文字列を生成します。
+	 * @returns {string} - ランダムに生成された一意の識別子GUID
+	 */
+	function randomGuid(): string;
+  }
