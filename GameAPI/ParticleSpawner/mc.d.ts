f09ffbda32d28a8558af835c5bc0d50c16d9904f
kaito02020424 <kaito02020424@gmail.com>
2023-11-04 14:45:59 +0900

    Add:mc translate

--- GameAPI/ParticleSpawner/mc.d.ts
+++ GameAPI/ParticleSpawner/mc.d.ts
@@ -1,11 +1,11 @@
 /// <reference path="../../index.d.ts" />
-declare namespace mc{
+declare namespace mc {
     /**
-     * ### 生成一个粒子生成器对象
+     * ### パーティクル生成オブジェクトを生成します
      * 
-     * @param displayRadius 默认值 `4294967295`
-     * @param highDetial 默认值 `true`
-     * @param doubleSide 默认值 `true`
+     * @param displayRadius デフォルト値：4294967295
+     * @param highDetail デフォルト値：true
+     * @param doubleSided デフォルト値：true
      */
-    function newParticleSpawner(displayRadius?:number , highDetial?:boolean , doubleSide?:boolean ):ParticleSpawner
-}
+    function newParticleSpawner(displayRadius?: number, highDetail?: boolean, doubleSided?: boolean): ParticleSpawner;
+}  
